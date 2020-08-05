9cbc44d9105678d9732230a2eef83709d100b3e6
MaetzigR <rm@isc-ub.de>
2020-08-05 08:17:03 +0200

    1 file modified

--- src/app/product-alert/product-alert.component.ts
+++ src/app/product-alert/product-alert.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 
 @Component({
-  selector: 'app-product-alert',
-  templateUrl: './product-alert.component.html',
-  styleUrls: ['./product-alert.component.css']
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
 })
+
 export class ProductAlertComponent implements OnInit {
 
   @Input() product; 
